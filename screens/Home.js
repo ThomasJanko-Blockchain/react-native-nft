5ee3f066c1fffef6238dd26d9de73178aa4f4020
ThomasJanko <thomas78125@gmail.com>
2023-06-16 22:40:52 +0200

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {View, StyleSheet, Text, SafeAreaView, FlatList} from 'react-native';
+
+
+import {COLORS, NFTData} from '../constants';
+
+const Home = () => {
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.black}}>
+            <Text>Home</Text>
+        </SafeAreaView>
+                    
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Home;
