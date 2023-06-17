56f77f1a01a0243091c7382501000fef8c936b94
ThomasJanko <thomas78125@gmail.com>
2023-06-17 10:59:20 +0200

    NFTTitle

---
+++ components/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, TouchableOpacity, Image} from 'react-native';
+import { COLORS, SIZES, FONTS, SHADOWS } from '../constants';
+
+
+export const CircleButton = ({imgUrl, handlePress, ...props}) => {
+    return (
+        <TouchableOpacity style={{
+            width: 40,
+            height: 40,
+            backgroundColor: COLORS.white,
+            position: 'absolute',
+            borderRadius: SIZES.extraLarge,
+            alignItems: 'center',
+            justifyContent: 'center',
+            ...SHADOWS.light,
+            ...props
+        }}>
+            <Image source={imgUrl} resizeMode='contain' style={{width: 24, height: 24}} />
+        </TouchableOpacity>
+    );
+}
+
+export const RectButton = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+
